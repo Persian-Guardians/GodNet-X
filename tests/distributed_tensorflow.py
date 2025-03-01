bf003ffcf3ab158da2d5887a38e3df2e274583e8
MSHADroo <MSHADroo@gmail.com>
2025-03-01 17:05:33 +0330

    commit

--- tests/distributed_tensorflow.py
+++ tests/distributed_tensorflow.py
@@ -2,14 +2,25 @@ import tensorflow as tf
 import os
 import json
 
+if tf.config.list_physical_devices('GPU'):
+    print("GodNet-X Metal GPU Detected 🔥")
+    tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
+else:
+    print("GodNet-X CPU Only")
+    
 os.environ["TF_CONFIG"] = '{"cluster": {"chief": ["192.168.31.73:12345"],"worker": ["192.168.31.100:12345"]},"task": {"type": "chief","index": "0"}}'
-
+    
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 BATCH_SIZE = 64
 BUFFER_SIZE = 10000
 tf_config = json.loads(os.environ["TF_CONFIG"])
 NUM_WORKERS = len(tf_config["cluster"]["worker"]) + 1  # +1 برای chief
+
+if tf_config["task"]["type"] == "worker":
+    print(f"GodNet-X Node Type: Worker-{tf_config['task']['index']}")
+else:
+    print("GodNet-X Node Type: Chief")
 
 (train_images, train_labels), _ = tf.keras.datasets.cifar10.load_data()
 train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
